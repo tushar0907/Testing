e5c3bf9e731d339220b9f25a7cae5ac722d2a30b
Tushar Kumar <tusharkumar0807@gmail.com>
2024-11-18 12:26:56 +0530

    chnages in gaps

--- src/app/(explore)/explore/page.tsx
+++ src/app/(explore)/explore/page.tsx
@@ -28,8 +28,8 @@ function ExplorePage() {
               </button>
             </Link> */}
           </div>
-          <div className="flex flex-wrap justify-between pb-4 mx-1">
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px] smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+          <div className="flex flex-wrap justify-between pb-4 smob:mx-1 mobile:mx-4 mx-1">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px] smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=cafes"}>
                 <img
                   src="https://b.zmtcdn.com/data/collections/088c7eea7df002b1fef0c1d257935be4_1709812426.png"
@@ -46,7 +46,7 @@ function ExplorePage() {
             </div>
 
             {/* Restaurants Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px] smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px] smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=restaurants"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/7/19209267/68a37ea737741f722465ea5a9eeee3f8_featured_v2.jpg"
@@ -62,7 +62,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Restobar Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px] smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px] smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=restobars"}>
                 <img
                   src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2a/8a/4a/5c/high-ultra-lounge.jpg?w=1200&h=-1&s=1"
@@ -78,7 +78,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Bars */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=bars"}>
                 <img
                   src="https://b.zmtcdn.com/data/collections/c67357df1d79854b624355b0272c5f28_1674826727.jpg"
@@ -94,7 +94,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Breweries */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=breweries"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/9/18843699/565978a181e8ae4ab11218dd423e1695.jpg?fit=around|750:500&crop=750:500;*,*"
@@ -110,7 +110,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Street Food Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=streetfood"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/0/20163010/1f9b38cab279bdeb05aa550855c700b1_featured_v2.jpg"
@@ -126,7 +126,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Dessert parlours Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=dessertparlors"}>
                 <img
                   src="https://b.zmtcdn.com/data/reviews_photos/211/135971f7ed24f3f4aba69e181e790211_1644785207.jpg?fit=around|750:500&crop=750:500;*,*"
@@ -142,7 +142,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Bakeries Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=bakeries"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/9/20398679/da06df6d48563e612b8a47ce457cf701.jpg?fit=around|750:500&crop=750:500;*,*"
@@ -158,7 +158,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Fine Dining Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=finedining"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/8/20930988/06f0e8b784b6a2f00f812f54aefcb8f7_featured_v2.jpg"
@@ -174,7 +174,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Casual Dining Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=casualdining"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/1/19890381/ca650ef58d7569c4368f99a23c76c6ca_featured_v2.jpg"
@@ -190,7 +190,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Buffet Restaurents Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=buffetrestaurants"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/2/19860482/e81f0e97e309897c69b2a4c2568bac65.jpg"
@@ -206,7 +206,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Quick Bites Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=quickbites"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/0/21418560/c74eb12c3948f1e412fd4416a668dd85_featured_v2.jpg"
@@ -222,7 +222,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Food Courts Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=foodcourts"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/chains/9/20439699/956ac9bf8427f9cf3fab6cab1d5ed389_featured_v2.jpg"
@@ -238,7 +238,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Theme based Eateries Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=themebasedeateries"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/6/19542856/79fee5c8522c9a02d4f60295f7d7d9a1.jpg?fit=around|750:500&crop=750:500;*,*"
@@ -254,7 +254,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Rooftop Dining Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=rooftopdining"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/1/55881/31f2201f7caf1632eeb947e6321f19f9.jpg?fit=around|750:500&crop=750:500;*,*"
@@ -270,7 +270,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Family Restaurants Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=familyfriendlyrestaurants"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/7/19511777/c384193195979a8b486fbd3d1c34e6ea_featured_v2.jpg"
@@ -286,7 +286,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Pet Friendly Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=petfriendlycafes"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/9/20996449/76db63ab6cca59adc9f1fea4f7823b5a_featured_v2.jpg"
@@ -302,7 +302,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Organic Eateries Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=organiceateries"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/1/19060291/e09672d91cac6380f81ebc58b3354bc0.jpg"
@@ -318,7 +318,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Ethnic Cuisines Restaurants Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=ethniccuisinerestaurants"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/1/20911451/15cd98a385c1e41f1ef7f7b1da0125b8_featured_v2.jpg"
@@ -334,7 +334,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Vegan and veg Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=veganvegetarianspots"}>
                 <img
                   src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/b0/fe/6b/aloft-rohit-dassani.jpg?w=600&h=-1&s=1"
@@ -351,7 +351,7 @@ function ExplorePage() {
             </div>
 
             {/* Winebars Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=winebars"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/7/21146027/06eeb4d5559246348992bcca302bd4c8.jpg"
@@ -367,7 +367,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Teahouses Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=teahouses"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/0/50510/8bcdb33346a4274049906b3178156b4d.jpg?fit=around|960:500&crop=960:500;*,*"
@@ -383,7 +383,7 @@ function ExplorePage() {
               </Link>
             </div>
             {/* Fast food Card */}
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=fastfoodchains"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/4/50684/321faaf587bef99094387b33e8128222.jpg?fit=around|750:500&crop=750:500;*,*"
@@ -398,7 +398,7 @@ function ExplorePage() {
                 </div>
               </Link>
             </div>
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=restaurants"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/7/19209267/68a37ea737741f722465ea5a9eeee3f8_featured_v2.jpg"
@@ -413,7 +413,7 @@ function ExplorePage() {
                 </div>
               </Link>
             </div>
-            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]">
+            <div className="flex-none my-2 mx-2 rounded-[8px] w-[300px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]">
               <Link href={"/exploredetails?category=restaurants"}>
                 <img
                   src="https://b.zmtcdn.com/data/pictures/7/19209267/68a37ea737741f722465ea5a9eeee3f8_featured_v2.jpg"
@@ -448,7 +448,7 @@ function ExplorePage() {
             {[...Array(6)].map((_, index) => (
               <div
                 key={index}
-                className="flex-none w-[300px] rounded-[8px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]"
+                className="flex-none w-[300px] rounded-[8px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]"
               >
                 <Link href={"/exploredetails"}>
                   <img
@@ -485,7 +485,7 @@ function ExplorePage() {
             {[...Array(6)].map((_, index) => (
               <div
                 key={index}
-                className="flex-none w-[300px] rounded-[8px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]"
+                className="flex-none w-[300px] rounded-[8px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]"
               >
                 <Link href={"/exploredetails"}>
                   <img
@@ -522,7 +522,7 @@ function ExplorePage() {
             {[...Array(6)].map((_, index) => (
               <div
                 key={index}
-                className="flex-none w-[300px] rounded-[8px] overflow-hidden relative tab:w-[250px]  smob:w-[160px] mobile:h-[220px] mobile:w-[175px]"
+                className="flex-none w-[300px] rounded-[8px] overflow-hidden relative tab:w-[250px]  smob:min-w-[150px] mobile:h-[220px] mobile:w-[170px]"
               >
                 <Link href={"/exploredetails"}>
                   <img
