bf142a4d2a89ee8ceb2eab125c518335ce26bd37
Tushar Kumar <tusharkumar0807@gmail.com>
2024-11-27 17:25:04 +0530

    chnages latest

--- src/app/newblog/blogdisplay/index.tsx
+++ src/app/newblog/blogdisplay/index.tsx
@@ -3,11 +3,11 @@ import BlogLayout from '../layoutt/index';
 
 const BlogDisplay: React.FC = () => {
   return (
-    <div className="flex w-full justify-around flex-wrap mx-auto p-6 space-y-6">
+    <div className="flex w-full justify-between flex-wrap mx-auto p-6 space-y-6">
       <BlogLayout
         title="Tired of trying the same food in Indiranagar" 
         description="So, here is a compact weekend guide for all those Indiranagar residents who are bored with the same places to visit. Presenting five restaurants that will give your taste buds a fresh new experience whenever you step out for something different.
-      </p>"
+      "
         imageUrl="https://placesinindia.com/wp/wp-content/uploads/2017/06/32594873194_d2e5700267_z.jpg"
         link="/blogs/blogtwo" // Link to the detailed blog page
       />
